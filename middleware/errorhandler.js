7c57765c8bd35540a10224f7d185700a44632275
mohit03nandan <mohitnandan81825@gmail.com>
2023-01-24 22:22:36 +0530

    added database config and initialzation

---
+++ middleware/errorhandler.js
@@ -0,0 +1,7 @@
+
+function Errorhandler(Error,req,res,next){
+    res.status(Error.status || 500);
+    res.send("internal sever error")
+}
+
+module.exports = Errorhandler
--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const express = require('express')
 const connect = require("./config/db")
+const Errorhandler = require("./middleware/errorhandler")
 const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 
 
 
 connect();
 
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(Errorhandler);
+
 app.get('/', (req, res) => {
     res.send("hello world")
 })
 
+
+app.get("/api/health" ,(req,res) =>{
+    res.send(`backend server is active status: active & time:${ new Date()}`)
+})
+
+
+// error handling
+app.use(function (req, res, next) {
+    res.status(404).send("Something went wrong! Please try after some time.");
+  })
 
 
 //connection part
